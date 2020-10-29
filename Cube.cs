8150581f1fc11ecb3da7c1bbaf04897a8fe24b85
MemeDealer5000 <46259677+MemeDealer5000@users.noreply.github.com>
2020-10-29 23:08:00 +0500

    Some simple inheritance samples

---
+++ Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    protected int HealthPoints;
+    protected float Velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Velocity = 2;
+        this.HealthPoints = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //doing nothing
+
+    }
+}
---
+++ CubePlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlayer : Cube
+{
+
+    private void OnCollisionEnter(Collision other)
+    {
+        this.HealthPoints = this.HealthPoints - 1;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log(this.HealthPoints);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Translate(Vector3.forward * Velocity * Time.deltaTime);
+            this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Translate(Vector3.forward * Velocity * Time.deltaTime);
+            this.gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+    }
+}
